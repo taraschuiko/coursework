a58d681255d26b9c696b1d067b0e825ea57e135f
taraschuiko <taraschuiko@gmail.com>
2019-04-26 13:22:04 +0300

    Made a matrix class to make life easier

--- src/Main.java
+++ src/Main.java
@@ -1,92 +1,16 @@
 import java.io.*;
-import java.util.Scanner;
 
 public class Main {
-
     public static void main(String[] args) throws IOException {
-        int[][] matrix = readMatrix("matrix.txt");
-
-        printMatrix(matrix);
-        matrix = sortMatrix(matrix);
-        printMatrix(matrix);
-        additionalTask(matrix);
+        Matrix matrix = new Matrix();
+        matrix.readFromFile("matrix.txt");
+        matrix.print();
+        matrix.sort();
+        matrix.print();
+        additionalTask(matrix.getMatrix());
     }
 
-    static int[][] readMatrix(String pathname) throws FileNotFoundException {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("N = ");
-        int n = Integer.parseInt(scanner.nextLine());
-        System.out.print("M = ");
-        int m = Integer.parseInt(scanner.nextLine());
-
-        int[][] matrix = new int[n][m];
-
-        File file = new File(pathname);
-        Scanner fileScanner = new Scanner(file);
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                matrix[i][j] = fileScanner.nextInt();
-            }
-        }
-
-        return matrix;
-    }
-
-    static void printMatrix(int[][] matrix) {
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[0].length; j++) {
-                System.out.print(matrix[i][j] + " ");
-            }
-            System.out.println();
-        }
-        System.out.println();
-    }
-
-    static int[][] sortMatrix(int[][] matrix) {
-        int min = matrix[0][0];
-        int max = matrix[0][0];
-
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[0].length; j++) {
-                if (matrix[i][j] < min) min = matrix[i][j];
-                if (matrix[i][j] > max) max = matrix[i][j];
-            }
-        }
-
-        for (int i = 0; i < matrix.length; i++) {
-            boolean hasMinOrMax = false;
-            for (int j = 0; j < matrix[0].length; j++) {
-                if (matrix[i][j] == min || matrix[i][j] == max) {
-                    hasMinOrMax = true;
-                    break;
-                }
-            }
-
-            if (hasMinOrMax) {
-                matrix[i] = sortArray(matrix[i]);
-            }
-        }
-
-        return matrix;
-    }
-
-    static int[] sortArray(int[] array) {
-        for (int i = 1; i < array.length; i++) {
-            int temp = array[i];
-            for (int j = i; j > 0; j--) {
-                if (array[j-1] > temp) {
-                    array[j] = array[j-1];
-                    array[j-1] = temp;
-                } else {
-                    break;
-                }
-            }
-        }
-
-        return array;
-    }
-
-    static void additionalTask(int[][] matrix) throws IOException {
+    private static void additionalTask(int[][] matrix) throws IOException {
         int countEvenElements = 0;
         int sumOfEvenElements = 0;
 
@@ -101,7 +25,6 @@ public class Main {
 
         int averageOfEvenElements = sumOfEvenElements / countEvenElements;
 
-        File file = new File("result.txt");
         BufferedWriter writer = new BufferedWriter(new FileWriter("result.txt"));
         writer.write("Середнє парних елементів: " + averageOfEvenElements);
         writer.close();
---
+++ src/Matrix.java
@@ -0,0 +1,97 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+class Matrix {
+    private int[][] matrix;
+
+    int[][] getMatrix() {
+        return this.matrix;
+    }
+
+    void readFromFile(String pathname) throws FileNotFoundException {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("N = ");
+        int n = Integer.parseInt(scanner.nextLine());
+        System.out.print("M = ");
+        int m = Integer.parseInt(scanner.nextLine());
+
+        int[][] matrix = new int[n][m];
+
+        File file = new File(pathname);
+        Scanner fileScanner = new Scanner(file);
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                matrix[i][j] = fileScanner.nextInt();
+            }
+        }
+
+        this.matrix = matrix;
+    }
+
+    void print() {
+        for (int i = 0; i < this.matrix.length; i++) {
+            for (int j = 0; j < this.matrix[0].length; j++) {
+                System.out.print(this.matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+    void sort() {
+        int min = findMinElement();
+        int max = findMaxElement();
+
+        for (int i = 0; i < this.matrix.length; i++) {
+            boolean hasMinOrMax = false;
+            for (int j = 0; j < this.matrix[0].length; j++) {
+                if (this.matrix[i][j] == min || this.matrix[i][j] == max) {
+                    hasMinOrMax = true;
+                    break;
+                }
+            }
+
+            if (hasMinOrMax) {
+                this.matrix[i] = sortRow(this.matrix[i]);
+            }
+        }
+    }
+
+    private int[] sortRow(int[] row) {
+        for (int i = 1; i < row.length; i++) {
+            int temp = row[i];
+            for (int j = i; j > 0; j--) {
+                if (row[j-1] > temp) {
+                    row[j] = row[j-1];
+                    row[j-1] = temp;
+                } else {
+                    break;
+                }
+            }
+        }
+
+        return row;
+    }
+
+    private int findMinElement() {
+        int min = this.matrix[0][0];
+        for (int i = 0; i < this.matrix.length; i++) {
+            for (int j = 0; j < this.matrix[0].length; j++) {
+                if (this.matrix[i][j] < min) min = this.matrix[i][j];
+            }
+        }
+        return min;
+    }
+
+    private int findMaxElement() {
+        int max = matrix[0][0];
+
+        for (int i = 0; i < this.matrix.length; i++) {
+            for (int j = 0; j < this.matrix[0].length; j++) {
+                if (this.matrix[i][j] > max) max = this.matrix[i][j];
+            }
+        }
+        return max;
+    }
+}
